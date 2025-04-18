895591748c46f3e58017d06d4c3fae26d7b1959a
andersbekkevard <anders.bekkevard@gmail.com>
2025-04-18 12:45:17 +0200

    Testcommit

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -4,8 +4,10 @@ package app;
  * Hello world!
  *
  */
+// hello world
 public class App {
 	public static void main(String[] args) {
+		System.out.println("Hello World!");
 		System.out.println("Hello World!");
 	}
 }
